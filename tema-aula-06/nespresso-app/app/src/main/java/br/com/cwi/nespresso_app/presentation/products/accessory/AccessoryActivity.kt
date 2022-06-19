6025a377f5dcc75c1b954bdbcd89be1019d605b9
Diogo Rodrigues <diogo2003rem@gmail.com>
2022-06-18 21:56:32 -0300

    adicionando tema da aula 6

---
+++ tema-aula-06/nespresso-app/app/src/main/java/br/com/cwi/nespresso_app/presentation/products/accessory/AccessoryActivity.kt
@@ -0,0 +1,12 @@
+package br.com.cwi.nespresso_app.presentation.products.accessory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.cwi.nespresso_app.R
+
+class AccessoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_accessory)
+    }
+}
---
+++ tema-aula-06/nespresso-app/app/src/main/java/br/com/cwi/nespresso_app/presentation/products/accessory/AccessoryAdapter.kt
@@ -0,0 +1,2 @@
+package br.com.cwi.nespresso_app.presentation.products.accessory
+
